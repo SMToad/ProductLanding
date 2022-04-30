17e2a920e26db09f3a4e2c5f847f487a228ff289
SMToad <97475628+SMToad@users.noreply.github.com>
2022-04-30 17:45:54 +0300

    Made the header shrink on scroll. Changed naming.

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if(window.innerWidth <= 700) return;
+  if (document.body.scrollTop > 40 || document.documentElement.scrollTop > 40) {
+    document.getElementsByClassName("header__img")[0].style.width = "50px";
+    document.getElementsByClassName("header__title")[0].style.fontSize = "25px";
+  } else {
+    document.getElementsByClassName("header__img")[0].style.width = "100px";
+    document.getElementsByClassName("header__title")[0].style.fontSize = "40px";
+  }
+}
 var slideIndex = 1;
 showSlides(slideIndex);
 
@@ -13,16 +25,16 @@ function currentSlide(n) {
 
 function showSlides(n) {
   var i;
-  var slides = document.getElementsByClassName("slides");
-  var dots = document.getElementsByClassName("dot");
+  var slides = document.getElementsByClassName("slideshow__slide");
+  var dots = document.getElementsByClassName("slideshow__dot");
   if (n > slides.length) {slideIndex = 1;}
   if (n < 1) {slideIndex = slides.length;}
   for (i = 0; i < slides.length; i++) {
     slides[i].style.display = "none";
   }
   for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
+    dots[i].className = dots[i].className.replace(" slideshow__dot_active", "");
   }
   slides[slideIndex - 1].style.display = "flex";
-  dots[slideIndex - 1].className += " active";
+  dots[slideIndex - 1].className += " slideshow__dot_active";
 }
